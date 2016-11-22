ba135ed7e3b93e76cc841dfb8c7bfd8f686fbc98
Galih Akbar Moerbayaksa <galnetwork.s@gmail.com>
2016-11-23 00:20:22 +0700

    Update getFollowers.php
    Change key and others encryption method!

--- getFollowers.php
+++ getFollowers.php
@@ -1,27 +1,9 @@
 <?php
 
-$get = new getFollowers();
-$get->id = "145457888"; // ID INSTAGRAMMU DISINI ! GAKUSAH SETTING" YANG LAIN CUK :)
-
-$get->signup(); // Ini register, biarin aja jangan dihapus karena penting!
-
-// Ini fungsi ambil coinnya, kreasikan!
-while(true){
-	$x = $get->getContents();
-	if(!empty($x['data']['followings'])){
-		foreach($x['data']['followings'] as $dfid){
-			$fid = $dfid['fid'];
-			$action = $get->startTask($fid);
-            echo $action['data']['credits'] . " ";
-		}
-	} else {
-		$dat = $get->signup();
-		echo "Session updated! ";
-	}
-}
+// Tutorial or Example doko ita no ?? :)
 
 class getFollowers {
-	
+
 	/*
 		Developer : Galih Akbar Moerbayaksa
 		Team : Kome-Ine Creative
@@ -55,14 +37,14 @@ class getFollowers {
 		return $data;
 	}
 	public function getContents(){
-		$content = '{"associate_id":"'.$this->id.'","app_id":302,"mid":"'.$this->mid.'","fetch_count":2,"sesn_id":"'.$this->session.'"}';
-		$base = 'https://ssafollow.api-alliance.com/follows/getfollowers/fetch?content='.$content.'&signature='.HASH::hmac($content).'&sig_kv=3';
+		$content = '{"associate_id":"'.$this->id.'","app_id":302,"mid":"'.$this->mid.'","fetch_count":1,"sesn_id":"'.$this->session.'"}';
+		$base = 'https://ssafollow.api-alliance.com/follows/getfollowers/fetch?content='.$content.'&signature='.HASH::hmac($content).'&sig_kv=5';
 		return json_decode(@file_get_contents($base), true);
 	}
 	public function orderCheck(){
-	    $content = '{"sesn_id":"'.$this->session.'","app_id":302,"associate_id":"'.$this->id.'","mid":"'.$this->mid.'"}';
-	    $base = 'https://ssafollow.api-alliance.com/follows/getfollowers/task/status?content='.$content.'&signature='.HASH::hmac($content).'&sig_kv=3';
-	    return json_decode(@file_get_contents($base), true);
+	    	$content = '{"sesn_id":"'.$this->session.'","app_id":302,"associate_id":"'.$this->id.'","mid":"'.$this->mid.'"}';
+	   	$base = 'https://ssafollow.api-alliance.com/follows/getfollowers/task/status?content='.$content.'&signature='.HASH::hmac($content).'&sig_kv=5';
+	    	return json_decode(@file_get_contents($base), true);
 	}
 	public function addOrder($total, $targetid, $targetUsername){
 		$content = '{"credits":'.($total*2).',"quantity":'.$total.',"tobefollow":{"fid":"'.$targetid.'","portrait":"https:\/\/scontent.cdninstagram.com\/t51.2885-19\/11906329_960233084022564_1448528159_a.jpg","username":"'.$targetUsername.'","private":0},"app_id":302,"associate_id":"'.$this->id.'","mid":"'.$this->mid.'","sesn_id":"'.$this->session.'"}';
@@ -88,15 +70,14 @@ class getFollowers {
 			$id_ig = $this->id;
 		}
 		$content = '{"assets":{"path":"\/asset\/v1\/query","basic":[]},"get_followers":{"unfollowed":0},"account_info":{"refrl":"Amazon"},"tp_info":{"acnt_typ":"instagram","sid":"'.$id_ig.'","client_verified":1},"app_id":302,"device_info":{"dvc_id":"'.UUID::get(true).'","enbl_ftur":"EnabledFeatures001Test","app_vrsn":"1.0.8","dvc_tkn":"APA91bFxd-6IdRSysefy5caXeMEVk4EHUX2Jpgildi7bTyULZmDPmmrfRIrCfD2tWcuQ3Qc7HdeQS_G-w2NvfWxUmJ5Jw7GYUCf48sg5vRqs_oMFQy-5c6EWPC2Z7JRU3mjHzJX5tJu-","dvc_typ":"android","usr_seg":"","app_grp":"nuunnnnnnnnnnnnnnu","dvc_lctn_set":0,"restrct_usr":false,"locl":"in_ID","dvc_os_vrsn":"4.4.2","dvc_tzone":25200},"associates":{}}';
-
 		$base = "https://ssafollow.api-alliance.com/follows/ssafollows/account/v1/signup";
 
 		$data = $this->http($base, $this->buildQuery($content));
 		$sessionid = $data['data']['main_account']['sesn_id'];
-	    $accesstoken = $data['data']['associates'][0]['access_token'];
-	    $id = $data['data']['associates'][0]['id'];
-	    $mid = $data['data']['main_account']['mid'];
-	    if(!empty($this->id)){
+	    	$accesstoken = $data['data']['associates'][0]['access_token'];
+	    	$id = $data['data']['associates'][0]['id'];
+	    	$mid = $data['data']['main_account']['mid'];
+	    	if(!empty($this->id)){
 			$this->mid = $mid;
 			$this->atok = $accesstoken;
 			$this->session = $sessionid;
@@ -111,7 +92,7 @@ class getFollowers {
 	        array(
 	            'content' => $content,
 	            'signature' => HASH::hmac($content),
-	            'sig_kv' => 3,
+	            'sig_kv' => 5,
 	            'cten' => 'p'
 	        )
 	    );
@@ -119,22 +100,22 @@ class getFollowers {
 	}
 	private function http($base, $content){
 		$opts = array('http' =>
-	        array(
-	            'method'  => 'POST',
-	            'header'  => 'Content-type: application/x-www-form-urlencoded',
-	            'content' => $content
-	        )
-	    );
-	    $context  = stream_context_create($opts);
-	    $result = @file_get_contents($base, false, $context);
-	    $result = json_decode($result, true);
-	    return $result;
+	        	array(
+	            		'method'  => 'POST',
+	            		'header'  => 'Content-type: application/x-www-form-urlencoded',
+	            		'content' => $content
+	        	)
+	    	);
+	    	$context  = stream_context_create($opts);
+	    	$result = @file_get_contents($base, false, $context);
+	    	$result = json_decode($result, true);
+	    	return $result;
 	}
 }
 
 class HASH {
 	
-	public $key = 'GU}ST90CA*>AsZyX;+81BtVR:!k $Q`6';
+	public $key = 'Z^`)t(OdE{|w2{`U.O=e.hVX&1[kX@iH';
 
 	static function hmac($data, $key = false){
 		$h = new HASH();
@@ -147,12 +128,12 @@ class HASH {
 class UUID {
 	static function get($type){
 		$uuid = sprintf('%04x%04x-%04x-%04x-%04x-%04x%04x%04x',
-          mt_rand(0, 0xffff), mt_rand(0, 0xffff),
-          mt_rand(0, 0xffff),
-          mt_rand(0, 0x0fff) | 0x4000,
-          mt_rand(0, 0x3fff) | 0x8000,
-          mt_rand(0, 0xffff), mt_rand(0, 0xffff), mt_rand(0, 0xffff)
-        );
-        return $type ? $uuid : str_replace('-', '', $uuid);
+			mt_rand(0, 0xffff), mt_rand(0, 0xffff),
+			mt_rand(0, 0xffff),
+			mt_rand(0, 0x0fff) | 0x4000,
+			mt_rand(0, 0x3fff) | 0x8000,
+         		mt_rand(0, 0xffff), mt_rand(0, 0xffff), mt_rand(0, 0xffff)
+        	);
+        	return $type ? $uuid : str_replace('-', '', $uuid);
 	}
 }
